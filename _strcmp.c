e33eb3ca13cb989a2fca2a1f3cc617d6ed9e214b
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-05-26 20:02:11 -0700

    file

--- _strcmp.c
+++ _strcmp.c
@@ -11,6 +11,7 @@
 
 int _strncmp(const char *s1, const char *s2, size_t len)
 {
+
 	int i = 0;
 
 	while (s1[i] != '\0' && s2[i] && len != 0)
@@ -19,6 +20,29 @@ int _strncmp(const char *s1, const char *s2, size_t len)
 			return (s1[i] - s2[i]);
 		i++;
 		len--;
+	}
+	return (0);
+}
+#include "main.h"
+
+/**
+ * _strcmp -  a function that compares two strings
+ * @s1: input value
+ * @s2: input value
+ * Return: Void
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
 	}
 	return (0);
 }
