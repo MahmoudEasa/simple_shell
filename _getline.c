b4d32dc00f930d18690af734c13fb17389695553
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-05-26 20:04:50 -0700

    file

--- _getline.c
+++ _getline.c
@@ -41,3 +41,22 @@ char *_getline()
 	return (buffer);
 }
 
+
+/**
+ * hashtag_handle - remove everything after #
+ * @buff: input;
+ * Return:void
+ */
+void hashtag_handle(char *buff)
+{
+	int i;
+
+		for (i = 0; buff[i] != '\0'; i++)
+		{
+			if (buff[i] == '#')
+			{
+			buff[i] = '\0';
+			break;
+			}
+	}
+}
