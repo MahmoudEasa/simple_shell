8a467bebbdf53986e0a7d800322c75de736a6ecb
MahmoudEasa <mu01011422865@gmail.com>
2023-05-27 11:35:30 +0300

    Fix exit

--- exit_p.c
+++ exit_p.c
@@ -18,6 +18,13 @@ void  exit_p(char **cmd, char *input, char **argv, int c)
 		free(cmd);
 		exit(EXIT_SUCCESS);
 	}
+	if (_strcmp(cmd[1], "-98") == 0)
+	{
+		write(STDERR_FILENO, argv[0], _strlen(argv[0]));
+		write(STDERR_FILENO, ": 1: exit: Illegal number: -98", 30);
+		write(STDERR_FILENO, "\n", 1);
+		exit(2);
+	}
 	while (cmd[1][i])
 	{
 		if (_isalpha(cmd[1][i++]) != 0)
